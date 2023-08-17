1fcc205158de779697f53d0b7095f5de6659d1df
Charlie <ickyicky@gmail.com>
2023-08-18 08:48:58 +0900

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,35 @@
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'C', function()
+  hs.application.launchOrFocus('Google Chrome')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'D', function()
+  hs.application.launchOrFocus('Discord')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'E', function()
+  hs.application.launchOrFocus('Finder')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'K', function()
+  hs.application.launchOrFocus('Kakaotalk')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'M', function()
+  hs.application.launchOrFocus('Mail')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'N', function()
+  hs.application.launchOrFocus('Notion')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'S', function()
+  hs.application.launchOrFocus('Slack')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'V', function()
+  hs.application.launchOrFocus('Visual studio code')
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'command'}, 'W', function()
+  hs.application.launchOrFocus('Warp')
+end)
